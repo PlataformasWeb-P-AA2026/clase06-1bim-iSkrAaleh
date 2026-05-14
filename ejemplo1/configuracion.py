1c3ed7d2e0365da81d17ad50ee6845df7fd0ff40
iSkrAaleh <alexsantiagoserranosaca@gmail.com>
2026-05-14 12:49:52 -0500

    hola

--- ejemplo1/configuracion.py
+++ ejemplo1/configuracion.py
@@ -4,13 +4,13 @@ from sqlalchemy import create_engine
 # para el ejemplo se usa la base de datos
 # sqlite
 
-engine = create_engine('sqlite:///demobase.db', echo=True)
+#engine = create_engine('sqlite:///demobase.db', echo=True)
 
 # mysql
 # pip install mysql-connector-python
-# engine = create_engine("mysql+mysqlconnector://user:pass@localhost:3306/demo100", echo=True)
+#engine = create_engine("mysql+mysqlconnector://user:password@localhost:3310/demo100")
 
 # postgres
 # sudo apt install libpq-dev
 # pip install psycopg2
-#engine = create_engine("postgresql+psycopg2://usuario:secreto@0.0.0.0:5432/mibase", echo=True)
+engine = create_engine("postgresql+psycopg2://user:password@localhost:5435/demo100")
